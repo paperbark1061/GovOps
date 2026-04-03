0de453e6b8808434b2001bd3966ec4c1e8b1773c
paperbark1061 <paperbark1061@duck.com>
2026-04-03 23:09:48 +1100

    Update DataService for 1,729 real panel companies across 8 data files

--- GovOps/Services/DataService.swift
+++ GovOps/Services/DataService.swift
@@ -42,7 +42,7 @@ class DataService: ObservableObject {
             }
 
             var loadedCompanies: [Company] = []
-            let compFiles = ["companies", "companies2", "companies3", "companies4"]
+            let compFiles = ["companies", "companies2", "companies3", "companies4", "companies5", "companies6", "companies7", "companies8"]
             for fileName in compFiles {
                 if let url = Bundle.main.url(forResource: fileName, withExtension: "json"),
                    let data = try? Data(contentsOf: url),
